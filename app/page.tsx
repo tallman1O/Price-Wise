6e559010c62e2de333edfb8f94846476985cd3ca
Mehul Uttam <mehulsavio@gmail.com>
2024-12-14 20:23:47 +0530

    Add HeroCarousel and SearchBar components to enhance homepage layout and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import HeroCarousel from "@/components/HeroCarousel";
+import SearchBar from "@/components/SearchBar";
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section className="px-6 md:px-20 py-24 ">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center">
+            <p className="small-text">
+              Smart Shopping Starts Here
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow"
+                width={16}
+                height={16}
+              />
+            </p>
 
-export default Home
+            <h1 className="head-text">
+              Unleash The Power Of{" "}
+              <span className="text-primary">PriceWise</span>
+            </h1>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you
+              convert, engage, and retain more.
+            </p>
+
+            <SearchBar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {['apple Iphone', 'Book', 'Banana'].map((item)=> (
+            <div key={item}>{item}</div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ components/SearchBar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+
+const SearchBar = () => {
+  const handleSubmit = () => {};
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter Product Link"
+        className="searchbar-input"
+      />
+
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
